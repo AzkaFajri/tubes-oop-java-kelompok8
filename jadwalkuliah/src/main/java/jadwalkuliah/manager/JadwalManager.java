3e2e38a4bce2e2703a435a804b57454a0afbaf3c
Muhammad Azka Fajri Maheswara <azkafajri965@gmail.com>
2025-12-22 16:44:44 +0700

    Create JadwalManager.java

---
+++ jadwalkuliah/src/main/java/jadwalkuliah/manager/JadwalManager.java
@@ -0,0 +1,29 @@
+package jadwalkuliah.manager;
+
+import java.util.*;
+
+import jadwalkuliah.exception.KonflikWaktuException;
+import jadwalkuliah.model.MataKuliah;
+
+public class JadwalManager {
+
+    private List<MataKuliah> daftar = new ArrayList<>();
+
+    public void tambah(MataKuliah mk) throws KonflikWaktuException {
+        for (MataKuliah m : daftar) {
+            if (m.getHari() == mk.getHari() &&
+               !(mk.getSelesai().isBefore(m.getMulai()) ||
+                 mk.getMulai().isAfter(m.getSelesai()))) {
+                throw new KonflikWaktuException(
+                    "Bentrok dengan " + m.getNama()
+                );
+            }
+        }
+        daftar.add(mk);
+    }
+
+    public List<MataKuliah> getSemua() {
+        return daftar;
+    }} {
+    
+}
